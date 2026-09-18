b3e633b66686a55d5791ae28fc45c889aeaec6ef
snordlund <nordlus@gmail.com>
2026-09-18 13:36:58 -0600

    Add outbox reply 0005-0005-ci-gitignore-fix.md

--- firmware/node-v1/.pio/build/esp32-s3/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3/FrameworkArduino/cbuf.cpp.o: \
- /home/astroboy/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/astroboy/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- firmware/node-v1/.pio/build/esp32-s3/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3/FrameworkArduino/libb64/cdecode.c.o: \
- /home/astroboy/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- /home/astroboy/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
