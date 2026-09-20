9799455389d86e168392bc452a2742bd159b2201
snordlund <nordlus@gmail.com>
2026-09-19 18:03:44 -0600

    feat: add lesson 01 timer-wake deep sleep for ESP32-S3

---
+++ firmware/lessons/lesson-01-timer-wake/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+RTC_DATA_ATTR int boot_count = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  boot_count++;
+  Serial.printf("boot #%d\n", boot_count);
+  Serial.println("sleeping 10s");
+  esp_deep_sleep_start();
+}
+
+void loop() {
+  // Should never reach here
+}
